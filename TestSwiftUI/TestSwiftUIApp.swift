99c859e167e262f70074ab05893cc9296c078395
Anh Tú <at.mrsteven@gmail.com>
2023-07-31 15:09:31 +0700

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by TuNA on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
